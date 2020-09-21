4907ad0984034a2d46ceb622750badb66dcaebc2
Keaton Sentak <k.sentak@outlook.com>
2020-09-21 01:43:30 -0400

    add calendar component and grab data from firebase

--- src/main.js
+++ src/main.js
@@ -1,26 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import VueTextareaAutosize from 'vue-textarea-autosize';
 // import firebase from 'firebase/app';
 // import 'firebase/firestore';
-// require('dotenv').config();
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
 
 Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false;
-
-// firebase.initializeApp({
-// 	apiKey: process.env.apiKey,
-// 	authDomain: process.env.authDomain,
-// 	databaseURL: process.env.databaseURL,
-// 	projectId: process.env.projectId,
-// 	storageBucket: process.env.storageBucket,
-// 	messagingSenderId: process.env.messagingSenderId,
-// 	appId: process.env.appId
-// });
-
-// export const db = firebase.firestore();
 
 new Vue({
 	vuetify,
