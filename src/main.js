29e9483305b7adfd129dedc2078495e147c54c6a
Keaton Sentak <k.sentak@outlook.com>
2020-09-21 00:38:19 -0400

    add initial calendar component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// require('dotenv').config();
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+// firebase.initializeApp({
+// 	apiKey: process.env.apiKey,
+// 	authDomain: process.env.authDomain,
+// 	databaseURL: process.env.databaseURL,
+// 	projectId: process.env.projectId,
+// 	storageBucket: process.env.storageBucket,
+// 	messagingSenderId: process.env.messagingSenderId,
+// 	appId: process.env.appId
+// });
+
+// export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	vuetify,
+	render: (h) => h(App)
+}).$mount('#app');
