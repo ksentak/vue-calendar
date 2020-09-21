3bfe2f4c18e65c03b080e6a310d7ab4ed3321b2a
Keaton Sentak <k.sentak@outlook.com>
2020-09-21 14:19:49 -0400

    add CRUD

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,6 +3,9 @@
 		<v-col>
 			<v-sheet height="64">
 				<v-toolbar flat color="white">
+					<v-btn color="primary" class="mr-4" @click="dialog = true">
+						Add Event
+					</v-btn>
 					<v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
 						Today
 					</v-btn>
@@ -40,6 +43,45 @@
 					</v-menu>
 				</v-toolbar>
 			</v-sheet>
+			<!-- Add Event Dialog -->
+			<v-dialog v-model="dialog" max-width="500">
+				<v-card>
+					<v-container>
+						<v-form @submit.prevent="addEvent">
+							<v-text-field
+								v-model="name"
+								type="text"
+								label="event name (required)"
+							></v-text-field>
+							<v-text-field v-model="details" type="text" label="detail"></v-text-field>
+							<v-text-field
+								v-model="start"
+								type="date"
+								label="start (required)"
+							></v-text-field>
+							<v-text-field
+								v-model="end"
+								type="date"
+								label="end (required)"
+							></v-text-field>
+							<v-text-field
+								v-model="color"
+								type="color"
+								label="color (click to open color menu)"
+							></v-text-field>
+							<v-btn
+								type="submit"
+								color="primary"
+								class="mr-4"
+								@click.stop="dialog = false"
+							>
+								Create Event
+							</v-btn>
+						</v-form>
+					</v-container>
+				</v-card>
+			</v-dialog>
+
 			<v-sheet height="600">
 				<v-calendar
 					ref="calendar"
@@ -61,24 +103,40 @@
 				>
 					<v-card color="grey lighten-4" min-width="350px" flat>
 						<v-toolbar :color="selectedEvent.color" dark>
-							<v-btn icon>
-								<v-icon>mdi-pencil</v-icon>
+							<v-btn @click="deleteEvent(selectedEvent.id)" icon>
+								<v-icon>mdi-delete</v-icon>
 							</v-btn>
 							<v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
 							<v-spacer></v-spacer>
-							<v-btn icon>
-								<v-icon>mdi-heart</v-icon>
-							</v-btn>
-							<v-btn icon>
-								<v-icon>mdi-dots-vertical</v-icon>
-							</v-btn>
 						</v-toolbar>
 						<v-card-text>
-							<span v-html="selectedEvent.details"></span>
+							<form v-if="currentlyEditing !== selectedEvent.id">
+								{{ selectedEvent.details }}
+							</form>
+							<form v-else>
+								<textarea-autosize
+									v-model="selectedEvent.details"
+									type="text"
+									style="width: 100%"
+									:min-height="100"
+									placeholder="add note"
+								>
+								</textarea-autosize>
+							</form>
 						</v-card-text>
 						<v-card-actions>
 							<v-btn text color="secondary" @click="selectedOpen = false">
-								Cancel
+								Close
+							</v-btn>
+							<v-btn
+								text
+								v-if="currentlyEditing !== selectedEvent.id"
+								@click.prevent="editEvent(selectedEvent)"
+							>
+								Edit
+							</v-btn>
+							<v-btn text v-else @click.prevent="updateEvent(selectedEvent)">
+								Save
 							</v-btn>
 						</v-card-actions>
 					</v-card>
